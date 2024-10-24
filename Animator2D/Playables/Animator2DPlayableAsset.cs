8990a9c3e3b78eebed10332e8937773cb639a312
Skrys33 <34647526+Skrys33@users.noreply.github.com>
2024-10-24 21:07:40 +0200

    Animator2DPlayableAsset

---
+++ Animator2D/Playables/Animator2DPlayableAsset.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Ankama.Animator2D.Playables
+{
+    [Serializable]
+    [DisplayName("Animator2D Clip")]
+    internal sealed class Animator2DPlayableAsset : PlayableAsset, ITimelineClipAsset
+    {
+        [SerializeField]
+        internal string animationName;
+
+        [SerializeField]
+        internal bool loop;
+
+        public ClipCaps clipCaps
+        {
+            get
+            {
+                return string.IsNullOrEmpty(animationName) ? ClipCaps.None : ClipCaps.Blending;
+            }
+        }
+
+        public override Playable CreatePlayable(PlayableGraph graph, GameObject owner)
+        {
+            throw new System.NotImplementedException("CreatePlayable method is not implemented in Animator2DPlayableAsset.");
+        }
+
+        public Animator2DPlayableAsset()
+        {
+            // Todo : Probably not the right implementation
+            // https://github.com/AlpaGit/waven-old/blob/6a3f33ee63b8cd7a02d42fb363cce675ccbab91b/Animator2D/Playables/Animator2DPlayableAsset.cs
+
+            if (string.IsNullOrEmpty(animationName))
+            {
+                animationName = "Default Animation";
+            }
+
+            // Todo : Core.Characters.Data.BonesLabelEventsIndexData$$.ctor(param_1, (MethodInfo*)0x0);
+
+            base.OnEnable();
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@
 - Reverse Ankama.Animator2D
 
 ## Progression
-9 file / (67 + 15?)
+10 file / (67 + 15?)
 
 
 There are many similarities with the Waven code.
