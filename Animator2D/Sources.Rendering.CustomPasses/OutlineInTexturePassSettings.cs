65c3b7ea3c31cd75c5842c6f5bf3af78cb0c4de4
Skrys33 <34647526+Skrys33@users.noreply.github.com>
2024-10-24 20:07:30 +0200

    OutlineInTexturePassSettings

---
+++ Animator2D/Sources.Rendering.CustomPasses/OutlineInTexturePassSettings.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace Ankama.Animator2D.Sources.Rendering.CustomPasses
+{
+
+    [Serializable]
+    public class OutlineInTexturePassSettings
+    {
+
+        public LayerMask layerMask;
+
+        public int shaderOutlinedInWhitePassIndex;
+
+        [Range(0f, 1f)]
+        public float alphaClip;
+
+        public OutlineInTexturePassSettings()
+        {
+            alphaClip = 0.6f;
+
+            // Todo : Ankama.Logging.Log$$Initialize(param_1);
+        }
+    }
+
+}
--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@
 - Reverse Ankama.Animator2D
 
 ## Progression
-6 file / (67 + 15?)
+7 file / (67 + 15?)
 
 
 There are many similarities with the Waven code.
