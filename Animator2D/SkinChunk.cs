1b4abfdad655f3eb83a359c98621f08029e12212
Skrys33 <34647526+Skrys33@users.noreply.github.com>
2024-10-28 00:09:50 +0100

    SkinChunk

---
+++ Animator2D/SkinChunk.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Ankama.Animations.Rendering;
+
+namespace Ankama.Animator2D
+{
+    [Serializable]
+    public struct SkinChunk
+    {
+        public int startVertexIndex;
+
+        public int indexCount;
+
+        public int startIndexIndex;
+
+        public int vertexCount;
+        
+        public int textureIndex;
+
+        public FlashMaskState maskState;
+    }
+
+}
--- readme.md
+++ readme.md
@@ -4,10 +4,10 @@
 - Reverse Ankama.Animator2D
 
 ## Progression
-20 files / (67 + 15?)
+22 files / (67 + 15?)
 
 ## Files
-- Animations (2/16)
+- Animations (3/16)
 - Animations.Events (10/11)
 - Animations.Management (0/1)
 - Animations.Playables (1/3)
