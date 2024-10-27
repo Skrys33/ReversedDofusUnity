5dd2899172db3fe75cdce57a257da76af2984f90
Skrys33 <34647526+Skrys33@users.noreply.github.com>
2024-10-28 00:07:40 +0100

    SkinTransformData

---
+++ Animator2D/SkinTransformData.cs
@@ -0,0 +1,19 @@
+﻿namespace Ankama.Animator2D
+{
+    public struct SkinTransformData
+    {
+        public float x;
+
+        public float y;
+
+        public float scaleX;
+
+        public float scaleY;
+
+        public short rotation;
+
+        public string originalClip;
+
+        public string overrideClip;
+    }
+}
--- readme.md
+++ readme.md
@@ -6,6 +6,13 @@
 ## Progression
 20 files / (67 + 15?)
 
+## Files
+- Animations (2/16)
+- Animations.Events (10/11)
+- Animations.Management (0/1)
+- Animations.Playables (1/3)
+- Animations.Rendering (5/31)
+- Animations.Sources.Rendering.CustomPasses (3/5)
 
 There are many similarities with the Waven code.
 
