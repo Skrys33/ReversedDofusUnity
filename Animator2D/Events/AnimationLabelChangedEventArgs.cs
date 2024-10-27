05aa87c3a5a5507a2f0b2ff7d20c1854a78cb46a
Skrys33 <34647526+Skrys33@users.noreply.github.com>
2024-10-27 23:47:07 +0100

    AnimationLabelChangedEventArgs

---
+++ Animator2D/Events/AnimationLabelChangedEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using JetBrains.Annotations;
+
+namespace Ankama.Animator2D.Events
+{
+    [PublicAPI]
+    public sealed class AnimationLabelChangedEventArgs : EventArgs
+    {
+        [PublicAPI] public string animation; // animation = dgmc
+
+        [PublicAPI] public string label; // label = dgmd
+
+        [PublicAPI]
+        public void bkfw(string animation, string label)
+        {
+            // Todo : It seems possible to merge with the constructor
+            this.animation = animation;
+            this.label = label;
+        }
+
+        public AnimationLabelChangedEventArgs()
+        {
+        }
+    }
+}
