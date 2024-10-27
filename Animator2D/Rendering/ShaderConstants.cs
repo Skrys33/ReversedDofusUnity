b8241d4eb139deacc4b25c163dd9ef5a9ba61e4d
Skrys33 <34647526+Skrys33@users.noreply.github.com>
2024-10-28 00:30:25 +0100

    ShaderConstants

---
+++ Animator2D/Rendering/ShaderConstants.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine.Rendering;
+
+namespace Ankama.Animator2D.Rendering
+{
+    public static class ShaderConstants
+    {
+        public const string UseColorEffectsKeyword = "USE_COLOR_EFFECTS";
+
+        public const string UseCustomisationColorsKeyword = "USE_CUSTOMISATION_COLORS";
+
+        public const string ApplyAlphaStencilKeyword = "APPLY_ALPHA_STENCIL";
+
+        public const string IsAnimator2DKeyword = "_ISANIMATOR2D";
+
+        public static readonly int blendSourceAlpha;
+
+        public static readonly int blendDestinationAlpha;
+
+        public const BlendMode DefaultAnimator2DBlendSourceFactorAlpha = BlendMode.OneMinusDstAlpha;
+
+        public const BlendMode DefaultAnimator2DBlendDestinationFactorAlpha = BlendMode.One;
+
+        public const string Color = "_Color";
+
+        public static LocalKeyword EnableHighlightKeyword;
+
+        public static LocalKeyword useCustomisationColorsKeyword;
+
+        public static LocalKeyword useColorEffectsKeywordKeyword;
+
+        public static LocalKeyword isAnimator2DKeyword;
+
+        public const string HighlightCoef = "_HighlightCoef";
+
+        public const string HighlightAdd = "_HighlightAdd";
+
+        public static LocalKeyword EnableOutlineKeyword;
+
+        public static LocalKeyword EnableOutlineSoloKeyword;
+
+        public static readonly int OutlineLayer;
+    }
+}
