7e4d3783d524b4ef93407da7156db9d06a9c6734
Skrys33 <34647526+Skrys33@users.noreply.github.com>
2024-10-28 23:02:29 +0100

    GraphicAsset

---
+++ Animator2D/GraphicAsset.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.InteropServices;
+using Ankama.Animator2D.Rendering;
+using UnityEngine;
+
+namespace Ankama.Animator2D
+{
+    public sealed class GraphicAsset : ScriptableObject
+    {
+        public Texture2D atlas;
+
+        public AnimationGeometryVertex[] vertices;
+
+        public ushort[] triangles;
+
+        public GraphicAsset() : base()
+        {
+            
+        }
+    }
+
+}
--- readme.md
+++ readme.md
@@ -4,10 +4,10 @@
 - Reverse Ankama.Animator2D
 
 ## Progression
-30 files / (67 + 15?)
+31 files / (67 + 15?)
 
 ## Files
-- Animations (8/16)
+- Animations (9/16)
 - Animations.Events (10/11)
 - Animations.Management (0/1)
 - Animations.Playables (1/3)
