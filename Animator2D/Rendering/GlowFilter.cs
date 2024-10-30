b9da941b4068fe2dc4e9c02f88ef770c1b2c8d25
Skrys33 <34647526+Skrys33@users.noreply.github.com>
2024-10-30 22:32:32 +0100

    GlowFilter

---
+++ Animator2D/Rendering/GlowFilter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+namespace Ankama.Animator2D.Rendering
+{
+    [Serializable]
+    public class GlowFilter : IEquatable<GlowFilter>
+    {
+        public Color32 glowColor;
+
+        public float blurX;
+
+        public float blurY;
+
+        public float strength;
+
+        public bool inner;
+
+        public bool knockout;
+
+        public bool compositeSource;
+
+        public int numPasses;
+
+        public bool Equals(GlowFilter other)
+        {
+            if (other == null)
+                return false;
+
+            return glowColor.Equals(other.glowColor) &&
+                   Mathf.Approximately(blurX, other.blurX) &&
+                   Mathf.Approximately(blurY, other.blurY) &&
+                   Mathf.Approximately(strength, other.strength) &&
+                   inner == other.inner &&
+                   knockout == other.knockout &&
+                   compositeSource == other.compositeSource &&
+                   numPasses == other.numPasses;
+        }
+
+        public GlowFilter()
+        {
+        }
+    }
+}
--- readme.md
+++ readme.md
@@ -4,14 +4,14 @@
 - Reverse Ankama.Animator2D
 
 ## Progression
-35 files / (67 + 15?)
+36 files / (67 + 15?)
 
 ## Files
 - Animations (13/19)
 - Animations.Events (10/11)
 - Animations.Management (0/1)
 - Animations.Playables (1/3)
-- Animations.Rendering (8/31)
+- Animations.Rendering (9/31)
 - Animations.Sources.Rendering.CustomPasses (3/5)
 
 There are many similarities with the Waven code.
